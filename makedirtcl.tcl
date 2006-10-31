8fb41602034a02c2e4d495345c6a280bd1bd6400
Peter.DeRijk <Peter.DeRijk@ua.ac.be>
2006-10-31 15:26:20 +0000

    update for tcl 8.4.14, buid in place, not conditional, add tk demos

--- makedirtcl.tcl
+++ makedirtcl.tcl
@@ -2,7 +2,7 @@
 # the next line restarts using tclsh \
 exec tclsh "$0" "$@"
 
-set tclversion 8.4.13
+set tclversion 8.4.14
 
 set platform $tcl_platform(platform)
 set basetcldir [file normalize ../tcl$tclversion]
@@ -19,6 +19,7 @@ if {$platform eq "unix"} {
 }
 
 set script [file normalize [info script]]
+# set script /home/peter/dev/dirtcl/makedirtcl.tcl
 if {"$tcl_platform(platform)"=="unix"} {
 	while 1 {
 		if {[catch {set script [file normalize [file readlink $script]]}]} break
@@ -26,22 +27,14 @@ if {"$tcl_platform(platform)"=="unix"} {
 }
 if {$script eq ""} {set scriptdir [pwd]} else {set scriptdir [file dir $script]}
 
-set todolist $argv
 set dirtcldir [pwd]
+
+if {[llength [glob -nocomplain $dirtcldir/*]]} {
+	error "error: current directory (in which dirtcl should be build) is not empty"
+}
 
 set dir [file dir [info script]]
 if {$dir ne {}} {cd $dir}
-
-proc todo {item} {
-	global todolist
-	if {$todolist eq ""} {
-		return 1
-	} elseif {[lsearch $todolist $item] != -1} {
-		return 1
-	} else {
-		return 0
-	}
-}
 
 proc file_read {file} {
 	set f [open $file]
@@ -109,8 +102,6 @@ static char initScript[] =
 #endif /* DIRTCL */
 }
 
-if {[todo convert]} {
-
 # code to do the conversion
 # -------------------------
 foreach dir [list $basetcldir/unix $basetcldir/win] {
@@ -175,64 +166,50 @@ set c [rewrite_after {"set tcl_libPath [list $tcl_library $tcl_root]\n"} $c {
 	"set tk_library [file join $tcl_root tk$tcl_version]\n"
 }]
 file_write $file $c
-}
 
-# prepare build directories
-# -------------------------
-if {[todo builddirs]} {
-catch {file delete -force $dirtcldir/dirtcl-build}
-catch {file mkdir $dirtcldir/dirtcl-build}
-catch {eval file delete -force [glob $dirtcldir/dirtcl/lib/tcl*]}
-catch {eval file delete -force [glob $dirtcldir/dirtcl/lib/tk*]}
-catch {eval file delete -force [glob $dirtcldir/dirtcl/lib/libtk*]}
-catch {eval file delete -force [glob $dirtcldir/dirtcl/apps/example*]}
-catch {eval file delete -force [glob $dirtcldir/dirtcl/apps/tkexample*]}
-catch {file mkdir $dirtcldir/dirtcl}
-catch {file mkdir $dirtcldir/dirtcl/lib}
-}
 
 # compile Tcl
 # -----------------------
 
-if {[todo compiletcl]} {
 puts "compiling tcl"
 cd $tcldir
 if {$platform eq "unix"} {
-	outexec sh ./configure --disable-shared --disable-threads --disable-symbols -prefix=$dirtcldir/dirtcl-build
+	outexec sh ./configure --disable-shared --disable-threads --disable-symbols -prefix=$dirtcldir
 } else {
-	outexec $sh ./configure --enable-shared --disable-threads --disable-symbols -prefix=$dirtcldir/dirtcl-build
+	outexec $sh ./configure --enable-shared --disable-threads --disable-symbols -prefix=$dirtcldir
 }
 catch {outexec make} e
 catch {outexec make install} e
-}
 
 # compile Tk
 # ----------------------
-if {[todo compiletk]} {
 puts "compiling tk"
 cd $tkdir
-outexec sh ./configure --enable-shared --disable-threads --disable-symbols --with-tcl=$tcldir -prefix=$dirtcldir/dirtcl-build
+outexec sh ./configure --enable-shared --disable-threads --disable-symbols --with-tcl=$tcldir -prefix=$dirtcldir
 catch {outexec make} e
 catch {outexec make install} e
-}
 
-# install Tcl
-# -----------
-if {[todo installtcl]} {
-puts "installing tcl"
-catch {eval file delete -force [glob -directory $dirtcldir/dirtcl/lib/ tcl* boot.tcl]}
-file copy -force [glob $dirtcldir/dirtcl-build/bin/tclsh*] $dirtcldir/dirtcl
-if {$platform eq "windows"} {
-	eval file copy -force [glob $dirtcldir/dirtcl-build/bin/*tcl*] $dirtcldir/dirtcl
+# Convert to dirtcl
+# -----------------
+puts "Converting to dirtcl"
+set files [glob $dirtcldir/bin/*]
+eval file rename $files $dirtcldir
+file delete $dirtcldir/bin
+if {$platform eq "unix"} {
+	cd $dirtcldir
+	set tcl [lindex [glob tcl*] 0]
+	catch {
+		set wish [lindex [glob wish*] 0]
+		eval file delete -force $wish
+		exec ln -s $tcl $wish
+	}
+	exec ln -s $tcl tclsh
+	exec ln -s $tcl wish
 }
-foreach dir [glob $dirtcldir/dirtcl-build/lib/tcl*] {
-	if {[file isdir $dir]} break
-}
-file copy -force $dir $dirtcldir/dirtcl/lib
-file copy -force [glob $dirtcldir/dirtcl-build/lib/tclConfig.sh] $dirtcldir/dirtcl/lib
+file delete -force $dirtcldir/man
 # write boot.tcl
-file copy $scriptdir/boot.tcl $dirtcldir/dirtcl/lib/boot.tcl
-set tcllibdir [glob $dirtcldir/dirtcl/lib/tcl[string index $tcl_version 0]*]
+file copy $scriptdir/boot.tcl $dirtcldir/lib/boot.tcl
+set tcllibdir [glob $dirtcldir/lib/tcl[string index $tcl_version 0]*]
 file copy $scriptdir/extension.tcl $tcllibdir
 set f [open $tcllibdir/tclIndex a]
 puts $f {set auto_index(extension) [list source [file join $dir extension.tcl]]}
@@ -255,62 +232,46 @@ set tcl_pkgPath [file join [file dirname [file dirname [info library]]] pkgs]
 set tclDefaultLibrary [info library]
 }} $c]
 file_write $tcllibdir/init.tcl $c
-}
+file mkdir $dirtcldir/pkgs
+file mkdir $dirtcldir/exts
+file copy [lindex [glob $scriptdir/pkgtools*] 0] $dirtcldir/exts
 
+
+# set ext for platform
+# --------------------
 if {$platform eq "windows"} {
 	set ext .exe
 } else {
 	set ext ""
 }
 
-# install Tk
-# ----------
-if {[todo installtk]} {
-puts "installing tk"
-catch {eval file delete -force [glob -directory $dirtcldir/dirtcl/lib/ tk*]}
-if {$platform eq "unix"} {
-	file copy -force [glob $dirtcldir/dirtcl-build/lib/libtk*[info sharedlibextension]] $dirtcldir/dirtcl/lib
-} else {
-	eval file copy -force [glob $dirtcldir/dirtcl-build/bin/tk*] $dirtcldir/dirtcl
-	eval file copy -force [glob $dirtcldir/dirtcl-build/bin/wish*] $dirtcldir/dirtcl
-}
-foreach dir [glob $dirtcldir/dirtcl-build/lib/tk*] {
-	if {[file isdir $dir]} break
-}
-file copy $dir $dirtcldir/dirtcl/lib
-file copy -force [glob $dirtcldir/dirtcl-build/lib/tkConfig.sh] $dirtcldir/dirtcl/lib
 if {$platform eq "windows"} {
 	file copy -force $tkdir/rc/wish.ico $dirtcldir/lib
 }
-}
-file mkdir $dirtcldir/dirtcl/pkgs
-file mkdir $dirtcldir/dirtcl/exts
+
 
 # setup example
 # -------------
-if {[todo tclexample]} {
 puts "setup example"
-catch {file mkdir $dirtcldir/dirtcl/apps/example}
-set f [open $dirtcldir/dirtcl/apps/example/example.tcl w]
+catch {file mkdir $dirtcldir/apps/example}
+set f [open $dirtcldir/apps/example/example.tcl w]
 puts $f {puts "Hello world: $tcl_interactive"
 }
 close $f
 if {$platform eq "unix"} {
 	set keeppwd [pwd]
-	cd $dirtcldir/dirtcl
-	exec ln -s [glob tclsh*] example$ext
+	cd $dirtcldir
+	exec ln -s [lindex [glob tclsh*] 0] example$ext
 	cd $keeppwd
 } else {
-	file copy -force [glob $dirtcldir/dirtcl/tclsh*] $dirtcldir/dirtcl/example$ext
-}
+	file copy -force [lindex [glob $dirtcldir/tclsh*] 0] $dirtcldir/example$ext
 }
 
 # setup tkexample
 # ---------------
-if {[todo tkexample]} {
 puts "setup tkexample"
-catch {file mkdir $dirtcldir/dirtcl/apps/tkexample}
-set f [open $dirtcldir/dirtcl/apps/tkexample/tkexample.tcl w]
+catch {file mkdir $dirtcldir/apps/tkexample}
+set f [open $dirtcldir/apps/tkexample/tkexample.tcl w]
 puts $f {package require Tk
 button .b -text "Hello world" -command {puts "Hello world"}
 pack .b -fill both -expand yes
@@ -318,11 +279,31 @@ pack .b -fill both -expand yes
 close $f
 if {$platform eq "unix"} {
 	set keeppwd [pwd]
-	cd $dirtcldir/dirtcl
-	exec ln -s [glob tclsh*] tkexample$ext
+	cd $dirtcldir
+	exec ln -s [lindex [glob tclsh*] 0] tkexample$ext
 	cd $keeppwd
 } else {
-	file copy -force [glob $dirtcldir/dirtcl/wish*] $dirtcldir/dirtcl/tkexample$ext
+	file copy -force [lindex [glob $dirtcldir/wish*] 0] $dirtcldir/tkexample$ext
 }
+
+# setup tkdemos
+# ---------------
+puts "setup tkexample"
+file copy $basetkdir/library/demos $dirtcldir/apps
+set f [open $dirtcldir/apps/demos/widget]
+set c [read $f]
+close $f
+set f [open $dirtcldir/apps/demos/demos.tcl w]
+puts $f "package require Tk"
+puts $f $c
+close $f
+if {$platform eq "unix"} {
+	set keeppwd [pwd]
+	cd $dirtcldir
+	exec ln -s [lindex [glob tclsh*] 0] demos$ext
+	cd $keeppwd
+} else {
+	file copy -force [lindex [glob $dirtcldir/wish*] 0] $dirtcldir/demos$ext
 }
+
 cd $dirtcldir
