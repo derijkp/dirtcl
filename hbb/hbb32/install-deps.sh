2317370511d6a3673b370edcb9f4d2050c4c5a21
Peter De Rijk <Peter.DeRijk@gmail.com>
2021-03-14 23:19:50 +0100

    install-deps.sh: add c++, devtoolset-8

--- hbb/hbb32/install-deps.sh
+++ hbb/hbb32/install-deps.sh
@@ -4,4 +4,5 @@ set -e
 echo "install-deps.sh"
 yum install -y yum-plugin-ovl
 yum install -y libX11-devel wget sudo
+yum install -y git gcc-c++
 echo "done"
--- hbb/hbb64/install-deps.sh
+++ hbb/hbb64/install-deps.sh
@@ -3,4 +3,11 @@ set -e
 
 echo "install-deps.sh"
 yum install -y libX11-devel wget sudo
+yum install -y git gcc-c++
+yum install -y centos-release-scl
+yum upgrade -y
+yum install -y devtoolset-8
+# you can now use
+# scl enable devtoolset-8 bash
+# to develop with a newer toolset
 echo "done"
